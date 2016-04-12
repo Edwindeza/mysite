5a03f470ae61df0d44fb04842a3b13bd1729af12
edwindeza <ejdeza@gmail.com>
2016-04-12 16:09:36 -0500

    Agregar vistas, template, y modelo details

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	# url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
 ]
